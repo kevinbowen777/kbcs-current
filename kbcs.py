7fb8dbc3adfe320c3f48cf5a9e344908278251bf
Kevin Bowen <kevin.bowen@gmail.com>
2016-08-16 11:19:04 -0700

    first commit

---
+++ kbcs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 12 17:38:16 2016
+Purpose: fetch currently playing track on http://kbcs.org
+and print to the console
+@author: kbowen (kevin.bowen@gmail.com)
+"""
+
+import requests
+
+r = requests.get('http://kbcsweb.bellevuecollege.edu/play/api/nowplaying/')
+
+now_playing = r.json()
+
+title = now_playing['title']
+playlistId = now_playing['playlistId']
+"""
+Some station programs do not populate data properly and return a
+playlistId of zero with no song information
+"""
+if playlistId == 0:
+    print ('==================================')
+    print 'KBCS 91.3 FM Radio  - ', title
+    print ('---------------------------------')
+    print ('Station Break.')
+    print 'Try again in a few minutes.'
+    print ('==================================')
+else:
+    artist = now_playing['artist']
+    title = now_playing['title']
+    print ('==================================')
+    print 'Now playing on KBCS 91.3 FM Radio -', title
+    print ('---------------------------------')
+    print 'Artist:', artist, '\n', 'Title: ', title
+    print ('==================================')
